7aabed4e56225e21253154a010ae68c8d9c98fe2
mbron770 <mbronfin.tsd@gmail.com>
2023-06-11 19:01:24 -0400

    add map

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  
+  // return tutorials.map(sentance => sentance.split(' ')
+  // .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+  // .join(' '))
+
+  return tutorials.map(sentence => sentence.split(' ')
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' '))
+  
 }
